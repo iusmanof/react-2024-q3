9ac9aa02831299d9caeac5899cff1c93fa97220d
iusmanof <iusmanof@gmail.com>
2024-07-07 13:46:26 -0700

    feat: implement 3 params from pokemon

--- src/components/SearchPanel/SearchInput.tsx
+++ src/components/SearchPanel/SearchInput.tsx
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 
 export default class SearchInput extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fetchApiData: "",
+      getApiData: { }
+    };
+  }
+
+  componentDidUpdate() {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${this.state.fetchApiData}`)
+      .then((respo) => {
+        return respo.json();
+      })
+      .then((data) => {
+        this.setState({ getApiData: { name: data.name, weight: data.weight, base_experience: data.base_experience }  });
+      });
+  }
+  fetchData() {
+    this.setState({ getApiData: this.state.fetchApiData });
+  }
+
+  handleChange = (event) => {
+    this.setState({ fetchApiData: event.target.value });
+  }
+
   render() {
     return (
       <div>
-        <input type="search" id="search-poremon" name="search-poremon" />
-        <button>Search pokemon</button>
+        <input type="search" id="search-poremon" name="search-poremon" onChange={this.handleChange}/>
+        <button onClick={() => this.fetchData()}>Search pokemon</button>
+        <p>Name : {this.state.getApiData.name}</p>
+        <p>weight : {this.state.getApiData.weight}</p>
+        <p>base_experience : {this.state.getApiData.base_experience}</p>
       </div>
     );
   }
--- src/components/SearchResult/SearchResult.tsx
+++ src/components/SearchResult/SearchResult.tsx
@@ -2,6 +2,6 @@ import React, { Component } from 'react';
 
 export default class SearchResult extends Component {
   render() {
-    return <div>SearchResult</div>;
+    return <div>Search data: </div>;
   }
 }
